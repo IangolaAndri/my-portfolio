c0cb5e8faa37154826e365e80f3eab421bb652de
Iangola Andrianarison <iangola.andrianarison@gmail.com>
2024-09-05 16:06:19 -0400

    initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section id="projects">
+      <h1>My projects</h1>
+      <p>Here are the projects that I have worked on.</p>
+    </section>
+  );
+}
+
+export default Projects;
