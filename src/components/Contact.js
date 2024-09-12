3885250a65801748882846710d5d41e66fcfd5b7
Iangola Andrianarison <iangola.andrianarison@gmail.com>
2024-09-11 21:04:51 -0400

    Update CSS styling and added projects and images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import LinkedIn from '../images/linkedin.png'
+import Github from '../images/github.png'
+import './Contact.css'
 
 function Contact() {
   return (
     <section id="contact">
       <h2>Contact me</h2>
-      <a href="https://www.linkedin.com/in/iangola-andrianarison-166a58271/" target="_blank">LinkedIn</a>&nbsp;&nbsp;
-      <a href="https://github.com/IangolaAndri" target="_blank">Github</a>
+      <a href="https://www.linkedin.com/in/iangola-andrianarison-166a58271/" target="_blank" rel="noopener noreferrer">
+        <img 
+            src={LinkedIn} 
+            alt="Clickable Example Image" 
+            style={{ cursor: 'pointer', width: '150px', height: '150px' }}
+        />
+        </a>
+      <a href="https://github.com/IangolaAndri" target="_blank" rel="noopener noreferrer">
+        <img 
+            src={Github} 
+            alt="Clickable Example Image" 
+            style={{ cursor: 'pointer', width: '150px', height: '150px' }}
+            />
+        </a>
+      <h2>iangola.andrianarison@gmail.com</h2>
     </section>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Projects.css'; 
+import Photography from '../images/Photography.png'
+import BrainHack from '../images/Brainhack.png'
+import Spotify from '../images/Spotify.png'
 
 function Projects() {
   return (
     <section id="projects">
       <h1>My projects</h1>
       <p>Here are the projects that I have worked on.</p>
+      <a href="https://analaogarchive.netlify.app/" target="_blank" rel="noopener noreferrer">
+        <img 
+            src={Photography} 
+            alt="Clickable Example Image" 
+            style={{ cursor: 'pointer', width: '800px', height: '400px' }}
+        />
+        </a>
+      <a href="https://school-brainhack.github.io/project/adhd-prediction/" target="_blank" rel="noopener noreferrer">
+        <img 
+            src={BrainHack} 
+            alt="Clickable Example Image" 
+            style={{ cursor: 'pointer', width: '800px', height: '400px' }}
+        />
+      </a>
+      <a href="https://github.com/IangolaAndri/Spotify-API-Project" target="_blank" rel="noopener noreferrer">
+        <img 
+            src={Spotify} 
+            alt="Clickable Example Image" 
+            style={{ cursor: 'pointer', width: '800px', height: '400px' }}
+        />
+        </a>
     </section>
   );
 }
